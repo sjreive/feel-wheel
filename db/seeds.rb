1979a6bd1914183d0b933383f366f77b2a4d52f7
Sarah Reive <sarahjreive@gmail.com>
2019-09-29 13:54:10 -0400

    Added DB seeds and namespaced controllers under /api/

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+feelings = Feeling.create([
+  {name: 'Happy', description: 'Characterized by well-being and contentment' }, 
+  {name: 'Sad', description: 'Affected with or expressive of grief or unhappiness'}, 
+  {name: 'Angry', description: 'A strong feeling of displeasure and usually of antagonism'},  
+  {name: 'Fearful', description: 'An unpleasant often strong emotion caused by anticipation or awareness of danger'},  
+  {name: 'Love', description: 'Strong affection and/or tenderness for another arising out of kinship, personal ties, admiration, benevolence, and/or desire'},  
+  {name: 'Disgusted', description: 'Marked aversion aroused by something highly distasteful'}, 
+  {name: 'Surprised', description: 'The feelings caused by something unexpected or unusual'}])
